6cc11a7b3837b8a58db55bed7899557557f67f1e
Li <1627843848@qq.com>
2022-06-28 19:53:35 +0800

    完成了翻转字符的案例

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
-  <div @click="fatherFn">
-    <!-- vue对事件进行了修饰符设置, 在事件后面.修饰符名即可使用更多的功能 -->
-    <button @click.stop="btn">.stop阻止事件冒泡</button>
-    <a href="http://www.baidu.com" @click.prevent="btn">.prevent阻止默认行为</a>
-    <button @click.once="btn">.once程序运行期间, 只触发一次事件处理函数</button>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="btn">点我翻转</button>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    fatherFn(){
-      console.log("father被触发");
-    },
+  data() {
+    return {
+      msg:"Hello,vue"
+    }
+  },
+  methods:{
     btn(){
-      console.log(1);
+      this.msg=this.msg.split('').reverse().join('')
     }
   }
 }
 </script>
+
+<style>
+
+</style>
