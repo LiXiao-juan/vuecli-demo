29d940de2ecc2473949583f38dea2dac4d010aa2
Li <1627843848@qq.com>
2022-06-28 20:30:27 +0800

    完成了练习

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,55 @@
 <template>
-  <div>
-    <input type="text" @input="inputFn" :value="val" />
+  <div class="container">
+    <div class="imglist">
+      <h3>图片列表区</h3>
+      <img src="./assets/01.png" alt="" @mouseenter="mouseenter(1)" @click="clickFn(1,$event)">
+      <img src="./assets/02.png" alt="" @mouseenter="mouseenter(2)" @click="clickFn(2,$event)">
+      <img src="./assets/03.png" alt="" @mouseenter="mouseenter(3)" @click="clickFn(3,$event)">
+    </div>
+    <div class="show">
+      <h3>图片预览区</h3>
+      <img :src="currentImg" alt="" style="width:600px;height:auto;">
+    </div>
+   <div>
+   </div>
   </div>
 </template>
 
 <script>
+import img01 from './assets/01.png'
+import img02 from './assets/02.png'
+import img03 from './assets/03.png'
 export default {
   data() {
     return {
-      val: '',
+      currentImg:img01
     }
   },
-  methods: {
-    inputFn(e) {
-      this.val = e.target.value
+  methods:{
+    mouseenter(val){
+      this.currentImg = {1:img01,2:img02,3:img03}[val]
     },
-  },
-}
+    clickFn(n,e){
+      alert(`当前是第${n}张，路径是${e.target.src}`)
+    }
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+  .container {
+    display:flex;
+  }
+ .imglist { 
+  border: 2px solid red;
+ }
+.imglist img {
+  width: 200px;
+  height: auto;
+}
+.show {
+  border: 2px solid red;
+  margin-right: 20px;
+}
+
+</style>
